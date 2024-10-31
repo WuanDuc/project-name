b539533713f4549bb7639f42aae149b281afb0b0
wuan.duc <univerbachtuoc@gmail.com>
2024-11-01 00:51:54 +0700

    add fuction to get top 10

--- src/students/students.controller.ts
+++ src/students/students.controller.ts
@@ -11,10 +11,23 @@ export class StudentController {
   async getStudentScoreBySbd(@Param('sbd') sbd: number): Promise<StudentEntity> {
     console.log(sbd);
     const student = await this.studentService.findStudentBySbd(sbd);
+    
     console.log(student);
     return student;
     //return this.studentService.findStudentById(sbd);
 
   }
   // get top student by subject
+  @Get('top10')
+  async getTop10StudentsInGroupA(): Promise<any[]> {
+    const result = await this.studentService.getTop10StudentsInGroupA();
+    return result;
+  }
+  // get number of student by score and by subject
+  @Get('reports/:subject')
+  async getReportBySubject(@Param('subject') subject: string): Promise<any> {
+    const result = await this.studentService.getReportBySubject(subject);
+    return result;
+  }
+
 }
--- src/students/students.service.ts
+++ src/students/students.service.ts
@@ -15,5 +15,39 @@ export class StudentsService {
         return student;  
         //return this.studentRepository.findOneBy({ sbd: id });
       }
-
+      async getReportBySubject(subject: string): Promise<any> {
+        const result = await this.studentRepository
+          .createQueryBuilder('student')
+          .select(`
+            COUNT(CASE WHEN ${subject} >= 8 THEN 1 END) AS level1,
+            COUNT(CASE WHEN ${subject} < 8 AND ${subject} >= 6 THEN 1 END) AS level2,
+            COUNT(CASE WHEN ${subject} < 6 AND ${subject} >= 4 THEN 1 END) AS level3,
+            COUNT(CASE WHEN ${subject} < 4 THEN 1 END) AS level4
+          `)
+          .getRawOne();
+    
+        return {
+          good: result.level1, // >= 8 points
+          mid: result.level2, // 6 <= points < 8
+          bad: result.level3, // 4 <= points < 6
+          fail: result.level4, // < 4 points
+        };
+      }
+      async getTop10StudentsInGroupA(): Promise<any[]> {
+        return await this.studentRepository
+        .createQueryBuilder('student')
+        .select('student.sbd')
+        .addSelect('student.toan')
+        .addSelect('student.vat_li')
+        .addSelect('student.hoa_hoc')
+        .addSelect(`
+            (student.toan + student.vat_li + student.hoa_hoc) / 3 AS avg_scores
+        `)
+        .where('student.toan IS NOT NULL')
+        .andWhere('student.vat_li IS NOT NULL')
+        .andWhere('student.hoa_hoc IS NOT NULL')
+        .orderBy('avg_scores', 'DESC')
+        .limit(10)
+        .getRawMany();
+    }
 }
